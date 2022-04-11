a804614d7321fb7ba4ab2a2b30e818b93c721a76
Josiah Lim <josiah.lim@ciit.edu.ph>
2022-04-12 03:57:20 +0800

    Changed camera background color and game display to 9:16 for mobile display. Added canvas and grid maker with random box and number spawner.

---
+++ Sudoku/Assets/Scripts/GridSquare.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSquare : Selectable
+{
+
+    [SerializeField] private GameObject number_text;
+    
+    private int number_ = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void DisplayText()
+    {
+        if (number_ <= 0)
+            number_text.GetComponent<Text>().text = " ";
+        else
+            number_text.GetComponent<Text>().text = number_.ToString();
+    }
+
+    public void SetNumber(int number)
+    {
+        number_ = number;
+        DisplayText();
+    }
+}
