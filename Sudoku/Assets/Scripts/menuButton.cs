cd6a334a93e324e85ab3962b83a98898fa6dd274
CentAlfaro <vincent.alfaro28@gmail.com>
2022-04-19 15:09:17 +0800

    fixed menuButton naming

--- Sudoku/Assets/Scripts/menuButton.cs
+++ Sudoku/Assets/Scripts/menuButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuButton : MonoBehaviour
+public class menuButton : MonoBehaviour
 {
    public void LoadScene(string name)
     {
